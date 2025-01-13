750432dd951486bbd49b7cb323416f48a489f920
Shakil <sayyadshakilgdsc@gmail.com>
2025-01-13 14:45:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def check_metadata():
         return jsonify({'error': 'Unable to check metadata existence or upload file.'}), 500
 
 if __name__ == '__main__':
-        app.run(debug=True, port=5000)
+        app.run(host='0.0.0.0', port=5000)
